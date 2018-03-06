c638f17e75c6591bb75edde4154806ca194c8923
MyMindWorld <gromovmx355@gmail.com>
2018-03-07 01:15:05 +0300

    improving
    Ignore cache,working func in evro

--- bank.py
+++ bank.py
@@ -1,16 +1,27 @@
 
 import account
 import evro
-val = cache 
-def main():
-    rate = int(input("Введите процентную ставку: "))
-    money = int(cache("Введите сумму: "))
-    period = int(input("Введите период ведения счета в месяцах: "))
 
+def main():
+    answer = input('"Вы хотите взять кредит в иностранной валюте? (y/n)')
+    if answer or answer[0].lower() != 'y':
+        money = int(input("Введите сумму, которую вы хотите обменять: "))
+        currency = int(input("Укажите код валюты (доллары - 400, евро - 401, болгарский лев - 402, венгерский форинт - 403): "))
+
+        result = evro.currency_calc(money,currency)
+
+    rate = int(input("Введите процентную ставку: "))
+    money = int(input("Введите сумму: "))
+    period = int(input("Введите период ведения счета в месяцах: "))
     result = account.calculate_income(rate, money, period)
     print("Параметры счета:\n", "Сумма: ", money, "\n", "Ставка: ", rate, "\n", "Период: ", period, "\n",
+              "Сумма на счете в конце периода: ", result)
+    else:
+        rate = int(input("Введите процентную ставку: "))
+        money = int(input("Введите сумму: "))
+        period = int(input("Введите период ведения счета в месяцах: "))
+
+        result = account.calculate_income(rate, money, period)
+        print("Параметры счета:\n", "Сумма: ", money, "\n", "Ставка: ", rate, "\n", "Период: ", period, "\n",
           "Сумма на счете в конце периода: ", result)
 
-
-if __name__ == "__main__":
-    main()
--- evro.py
+++ evro.py
@@ -1,25 +1,33 @@
-usd = 57
-euro = 60
-bgn = 35
-huf = 22
+def currency_calc(money,currency):
+    usd = 57
+    euro = 60
+    bgn = 35
+    huf = 22
+    if currency == 400:
+        money = round(money / usd, 2)
+        print("Валюта: доллары США")
+    elif currency == 401:
+        money = round(money / euro, 2)
+        print("Валюта: евро")
+    elif currency == 402:
+        money = round(money / bgn, 2)
+        print("Валюта: болгарский лев")
+    elif currency == 403:
+        money = round(money / huf, 2)
+        print("Валюта: венгерский форинт")
+    else:
+        money = 0
+        print("Неизвестная валюта")
+    return money
 
-money = int(input("Введите сумму, которую вы хотите обменять: "))
-currency = int(input("Укажите код валюты (доллары - 400, евро - 401, болгарский лев - 402, венгерский форинт - 403): "))
+def main():
+    money = 100
+    currency = 401
 
-if currency == 400:
-    cache = round(money / usd, 2)
-    print("Валюта: доллары США")
-elif currency == 401:
-    cache = round(money / euro, 2)
-    print("Валюта: евро")
-elif currency == 402:
-    cache = round(money / bgn, 2)
-    print("Валюта: болгарский лев")
-elif currency == 403:
-    cache = round(money / huf, 2)
-    print("Валюта: венгерский форинт")
-else:
-    cache = 0
-    print("Неизвестная валюта")
+    result = currency_calc(money,currency)
+    print("курс:", result)
 
-print("К получению:", cache)
+
+if __name__ == "__main__":
+    main()
+
