a8fc668b07c65d6ffd3e4b3debb33d02e12349db
MyMindWorld <gromovmx355@gmail.com>
2018-05-08 15:37:26 +0300

    PythonChallenge
    6

--- PythonChallenge/6.py
+++ PythonChallenge/6.py
@@ -1,18 +1,31 @@
-import zipfile, re, string
+import zipfile
+import string
 file = zipfile.ZipFile("channel.zip")
 num = '90052'
-newnum = ''
-
+collect = 0
+comments = []
 def cleanraw(nextraw):
     print(nextraw)
     newnum = ''
     for i in nextraw:
         if i in string.digits:
             newnum += str(i)
+    if newnum == '':
+        return 0
     return(newnum)
 
+
+
 nextraw = (file.read('%s.txt' % num).decode('utf-8'))
-cleanraw(nextraw)
+comments.append(str((file.getinfo('%s.txt' % num).comment.decode("utf-8"))))
+print('nextraw', nextraw)
+newnum = cleanraw(nextraw)
 while True:
+    if newnum == 0:
+        print(collect)
+        print("".join(comments))
+        break
+    collect += int(newnum)
     nextraw = (file.read('%s.txt' % newnum).decode('utf-8'))
-    cleanraw(nextraw)
+    comments.append(str((file.getinfo('%s.txt' % newnum).comment.decode("utf-8"))))
+    newnum = cleanraw(nextraw)
